1064d381f9efd29c69eb380bbc7dee163e1aa091
w3cdpass <killu1875@gmail.com>
2024-12-09 11:19:05 +0530

    Update files

---
+++ DSA/index.js
@@ -0,0 +1,30 @@
+// Write a program to
+// reverse a string without using built-in functions.
+
+
+// function reStr(str) {
+//     return
+// }
+
+
+// var s = str[]
+// for (i = 0; i >= str; i++) {
+//     console.log(str)
+// }
+
+
+var str = "gamer";
+var rev = "";
+for (var i = str.length - 1; i >= 0; i--) {
+    var g = rev += str[i];
+}
+
+console.log(rev)
+
+// var sta = "";
+// for (var i = str.length + 1; i >= 0; i++){
+//     var a = sta += str[i]
+// };
+
+
+// console.log(sta)
